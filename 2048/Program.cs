74271e766ac8b5369522082aaacceec5afff8c8d
Noodles94 <31862788+Noodles94@users.noreply.github.com>
2019-10-14 11:12:56 +0200

    Add files via upload

---
+++ 2048/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Program
+    {
+        private int[,] gameField = new int[4, 4];
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.load();
+            program.run();
+        }
+        public void load()
+        {
+            for(int i = 0; i < gameField.GetLength(0); ++i)
+            {
+                for(int j = 0; j < gameField.GetLength(1); ++j)
+                {
+                    gameField[i, j] = 0;
+                    Console.Write(gameField[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            
+            Console.ReadLine();
+        }
+        public void run()
+        {
+            
+        }
+    }
+}
