084e622ad5ea739e97f7b5c2f51a6354fc3c3d7e
Noodles94 <31862788+Noodles94@users.noreply.github.com>
2019-10-20 09:53:59 +0200

    Add files via upload

---
+++ Eating out/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EatingOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nrs = Console.ReadLine();
+            String[] nrsA = nrs.Split(' ');
+            int[] ab = new int[nrsA.Length];
+            for (int i = 0; i < nrsA.Length; ++i)
+            {
+                ab[i] = Int32.Parse(nrsA[i]);
+            }
+            int m = ab[0];
+            int a = ab[1];
+            int b = ab[2];
+            int c = ab[3];
+            if ((a+ b +c)> (m*2))
+            {
+                Console.WriteLine("impossible");
+
+            }
+            else
+            {
+                Console.WriteLine("possible");
+            }
+            Console.ReadLine();
+        }
+    }
+}
